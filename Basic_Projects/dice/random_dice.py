9438831a6497718e61b46b91ae160e9b165fb9ce
KriskoAGM <christiqn.iordanow96@gmail.com>
2022-07-17 01:06:19 +0300

    dice with function

--- Basic_Projects/dice/random_dice.py
+++ Basic_Projects/dice/random_dice.py
@@ -1,52 +1,51 @@
 import random
 
+
+def dice(dice_num):
+    if dice_number == 1:
+        print("+ - - - - +")
+        print("|         |")
+        print("|    0    |")
+        print("|         |")
+        print("+ - - - - +")
+    elif dice_number == 2:
+        print("+ - - - - +")
+        print("|0        |")
+        print("|         |")
+        print("|        0|")
+        print("+ - - - - +")
+    elif dice_number == 3:
+        print("+ - - - - +")
+        print("| 0       |")
+        print("|    0    |")
+        print("|        0|")
+        print("+ - - - - +")
+    elif dice_number == 4:
+        print("+ - - - - +")
+        print("|0       0|")
+        print("|         |")
+        print("|0       0|")
+        print("+ - - - - +")
+    elif dice_number == 5:
+        print("+ - - - - +")
+        print("|0       0|")
+        print("|    0    |")
+        print("|0       0|")
+        print("+ - - - - +")
+    elif dice_number == 6:
+        print("+ - - - - +")
+        print("|0   0   0|")
+        print("|         |")
+        print("|0   0   0|")
+        print("+ - - - - +")
+
+
 while True:
-    command = input("Press 'R' to roll the dice or 'Q' to quit:").lower()
+    command = input("Press 'R' to roll the dice or 'Q' to quit: ").lower()
 
     if command == "r":
         dice_number = random.randint(1, 6)
-
-        if dice_number == 1:
-            print("+ - - - - +")
-            print("|         |")
-            print("|    0    |")
-            print("|         |")
-            print("+ - - - - +")
-
-        elif dice_number == 2:
-            print("+ - - - - +")
-            print("|0        |")
-            print("|         |")
-            print("|        0|")
-            print("+ - - - - +")
-
-        elif dice_number == 3:
-            print("+ - - - - +")
-            print("| 0       |")
-            print("|    0    |")
-            print("|        0|")
-            print("+ - - - - +")
-
-        elif dice_number == 4:
-            print("+ - - - - +")
-            print("|0       0|")
-            print("|         |")
-            print("|0       0|")
-            print("+ - - - - +")
-
-        elif dice_number == 5:
-            print("+ - - - - +")
-            print("|0       0|")
-            print("|    0    |")
-            print("|0       0|")
-            print("+ - - - - +")
-
-        elif dice_number == 6:
-            print("+ - - - - +")
-            print("|0   0   0|")
-            print("|         |")
-            print("|0   0   0|")
-            print("+ - - - - +")
+        dice(dice_number)
 
     elif command == "q":
         break
