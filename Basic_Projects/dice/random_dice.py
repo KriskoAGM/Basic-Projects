43ef794f317d3dc89c3f7134ca1369f6084733ef
KriskoAGM <christiqn.iordanow96@gmail.com>
2022-07-16 22:12:51 +0300

    My basic python projects

---
+++ Basic_Projects/dice/random_dice.py
@@ -0,0 +1,52 @@
+import random
+
+while True:
+    command = input("Type 'Y' to roll the dice or 'N' to quit:").lower()
+
+    if command == "y":
+        dice_number = random.randint(1, 6)
+
+        if dice_number == 1:
+            print("+ - - - - +")
+            print("|         |")
+            print("|    0    |")
+            print("|         |")
+            print("+ - - - - +")
+
+        elif dice_number == 2:
+            print("+ - - - - +")
+            print("|0        |")
+            print("|         |")
+            print("|        0|")
+            print("+ - - - - +")
+
+        elif dice_number == 3:
+            print("+ - - - - +")
+            print("| 0       |")
+            print("|    0    |")
+            print("|        0|")
+            print("+ - - - - +")
+
+        elif dice_number == 4:
+            print("+ - - - - +")
+            print("|0       0|")
+            print("|         |")
+            print("|0       0|")
+            print("+ - - - - +")
+
+        elif dice_number == 5:
+            print("+ - - - - +")
+            print("|0       0|")
+            print("|    0    |")
+            print("|0       0|")
+            print("+ - - - - +")
+
+        elif dice_number == 6:
+            print("+ - - - - +")
+            print("|0   0   0|")
+            print("|         |")
+            print("|0   0   0|")
+            print("+ - - - - +")
+    else:
+        break
+
